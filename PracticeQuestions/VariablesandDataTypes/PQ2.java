016849c9a6ba09767dc9c9bc2a5d4b254b307ddd
tina <102860264+tinachelwanii@users.noreply.github.com>
2024-01-11 19:46:40 +0530

    Add files via upload

---
+++ PracticeQuestions/VariablesandDataTypes/PQ2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class PQ2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int side = sc.nextInt();
+        int area = side * side;
+        System.out.println(area);
+        sc.close();
+    }
+}

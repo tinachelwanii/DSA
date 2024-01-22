35e9bb346a225d5ce65f2a40c96624cef88ce810
tina <102860264+tinachelwanii@users.noreply.github.com>
2024-01-22 16:55:42 +0530

    Add files via upload

---
+++ Operators/Operator10.java
@@ -0,0 +1,21 @@
+//assignment operators video
+//import java.util.*;
+public class Operator10 {
+    public static void main (String args[]) { 
+        int A = 10;
+         A+=10; 
+         int B = 10;
+         B-=5;
+         int C = 5;
+         C*=5;
+         int D = 5;
+         D/=5;
+         int E = 5;
+         E%=5;
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+        System.out.println(D);
+        System.out.println(E);
+    }
+}

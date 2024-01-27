7d019354b8d2ee861cd9c9a6e1daf438718d03a4
tina <102860264+tinachelwanii@users.noreply.github.com>
2024-01-27 18:13:18 +0530

    Create ConditionalStatements1.java

---
+++ ConditionalStatements/ConditionalStatements1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class ConditionalStatements1 {
+    public static void main(String args[]) {
+        int age = 16;
+        if (age > 18) {
+            System.out.println("adult : drive,vote");
+        }
+
+        if (age > 13 && age < 18) {
+            System.out.println("teenager");
+        }
+        else {
+            System.out.println("not adult");
+        }
+    }
+}

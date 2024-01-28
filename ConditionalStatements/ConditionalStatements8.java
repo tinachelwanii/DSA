f6fd5e42a001c11b09ad4fa0b142e453dd927fa9
tina <102860264+tinachelwanii@users.noreply.github.com>
2024-01-28 23:24:31 +0530

    Add files via upload

---
+++ ConditionalStatements/ConditionalStatements8.java
@@ -0,0 +1,8 @@
+//check if a student will pass or fail
+public class ConditionalStatements8 {
+    public static void main(String[] args) {
+        int marks = 67;
+        String reportcard= marks >= 33 ? "pass":"fail";
+        System.out.println(reportcard);
+    }
+}

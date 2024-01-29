d5400fad131acb68b575b7ec321bff5c02505ac2
tina <102860264+tinachelwanii@users.noreply.github.com>
2024-01-29 23:56:18 +0530

    Add files via upload

---
+++ Loops/Loops2.java
@@ -0,0 +1,12 @@
+//print number from 1 to 10
+//import java.util.*;
+public class Loops2 {
+    public static void main(String[] args) {
+        int counter = 1;
+        while (counter < 10) {
+            System.out.print(counter+ " ");
+            counter++;
+        }
+        System.out.println();
+    }
+}

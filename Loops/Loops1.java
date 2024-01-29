b425c82e0fa62e7839e114ebbde5591a5e9a48fc
tina <102860264+tinachelwanii@users.noreply.github.com>
2024-01-29 10:41:05 +0530

    Create Loops1.java

---
+++ Loops/Loops1.java
@@ -0,0 +1,13 @@
+//while loop 
+//print hello world 100 times 
+import java.util.*;
+public class Loops1 {
+    public static void main(String[] args) {
+        int counter = 0;
+        while(counter < 100) {
+        System.out.println("Hello World");
+        counter++;
+        }
+        System.out.println("printed hw 100 times");
+    }
+}

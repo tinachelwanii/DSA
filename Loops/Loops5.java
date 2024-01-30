c4cf0ac7b5e48d626463cc8bdcd0fa07013abfb7
tina <102860264+tinachelwanii@users.noreply.github.com>
2024-01-30 23:51:46 +0530

    Add files via upload

---
+++ Loops/Loops5.java
@@ -0,0 +1,8 @@
+//for loop video
+//import java.util.*;
+public record Loops5() {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++ )
+        System.out.println("Hello World");
+    }
+}
---
+++ Loops/Loops6.java
@@ -0,0 +1,6 @@
+public class Loops6 {
+    public static void main(String[] args) {
+        for (int line=1; line<=4; line++)
+        System.out.println("****");
+    }
+}

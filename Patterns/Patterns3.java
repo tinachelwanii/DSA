4302c86dddafc3df169c0ebe768794f6f4003def
tina <102860264+tinachelwanii@users.noreply.github.com>
2024-01-31 22:22:16 +0530

    Add files via upload

---
+++ Patterns/Patterns3.java
@@ -0,0 +1,13 @@
+//print half pyramid pattern
+public class Patterns3 {
+    public static void main(String[] args) {
+        int n=4;
+        for(int line=1;line<=n;line++){
+            //number print
+        for(int number=1;number<=line;number++) {
+            System.out.print(number);
+        }
+        System.out.println();
+    }
+    }
+}

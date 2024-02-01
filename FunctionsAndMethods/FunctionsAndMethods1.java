c961aacbf73c915aa437f234c68704bf69e834c5
tina <102860264+tinachelwanii@users.noreply.github.com>
2024-02-01 09:40:16 +0530

    Create FunctionsAndMethods1.java

---
+++ FunctionsAndMethods/FunctionsAndMethods1.java
@@ -0,0 +1,12 @@
+//syntax function code demo
+public class FunctionsAndMethods1 {
+    public static void printHelloWorld() {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        return;
+    }
+    public static void main(String args[]) {
+    printHelloWorld(); //function call
+   }
+}

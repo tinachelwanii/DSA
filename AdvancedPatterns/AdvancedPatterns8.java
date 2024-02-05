58cbadaae098c6fb0be640787b473a33dead2e3f
tina <102860264+tinachelwanii@users.noreply.github.com>
2024-02-05 09:47:12 +0530

    Add files via upload

---
+++ AdvancedPatterns/AdvancedPatterns8.java
@@ -0,0 +1,23 @@
+public class AdvancedPatterns8 {
+    public static void hollowrhombus(int n) {
+       for(int i=1;i<=n;i++) {
+        //spaces
+        for(int j=1;j<=(n-i);j++) {
+            System.out.print(" ");
+        }
+
+        //hollow rectangle
+        for(int j=1;j<=n;j++) {
+            if(i == 1 || i == n || j == 1 || j == n) {
+                System.out.print("*");
+            } else {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+       }        
+    }
+    public static void main(String[] args) {
+        hollowrhombus(5);
+    }
+}

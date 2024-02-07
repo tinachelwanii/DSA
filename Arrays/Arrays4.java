d68f2858264923e62a9b5af0bbb10655b8b452eb
tina <102860264+tinachelwanii@users.noreply.github.com>
2024-02-07 10:07:30 +0530

    Add files via upload

---
+++ Arrays/Arrays4.java
@@ -0,0 +1,18 @@
+//print largest number
+//import java.util.*;
+public class Arrays4 {
+    public static int getLargest(int numbers[]) {
+        int largest = Integer.MIN_VALUE; // -infinity
+
+    for(int i=0; i<numbers.length;i++) {
+        if(largest<numbers[i]){
+            largest=numbers[i];
+        }
+    }
+    return largest;
+    }
+    public static void main(String[] args) {
+        int numbers[] = {1,2,6,3,5};
+        System.out.println("largest number is: " + getLargest(numbers));
+    }
+}

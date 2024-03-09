ae8de9361d045166e7ed981f52dd6794bf9b5cbc
tina <102860264+tinachelwanii@users.noreply.github.com>
2024-03-09 23:48:27 +0530

    Create BitManipulation1.java

---
+++ BitManipulation/BitManipulation1.java
@@ -0,0 +1,10 @@
+//Bit-wise Operators
+public class BitManipulation1 {
+    public static void main(String[] args) {
+        System.out.println(5&6);
+        System.out.println(5 | 6);
+        System.out.println(5 ^ 6);
+        System.out.println(5<<2);
+        System.out.println(6>>1);
+    }
+}

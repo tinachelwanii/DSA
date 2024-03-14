705207610c1cc7b539711a3f1b5eb8bb553e167f
tina <102860264+tinachelwanii@users.noreply.github.com>
2024-03-14 12:18:44 +0530

    Add files via upload

---
+++ Recursion/Recursion2.java
@@ -0,0 +1,15 @@
+//print numbers in increasing order
+public class Recursion2 {
+    public static void printInc(int n){
+        if(n==1){
+            System.out.print(n+" ");
+            return;
+        }
+        printInc(n-1);
+        System.out.print(n+" ");
+    }
+    public static void main(String args[]){
+        int n=10;
+        printInc(n);
+    }
+}
